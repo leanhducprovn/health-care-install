ff7f55c237fa9991457dc13191623e327d4da8e9
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-18 22:14:51 +0700

    update

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-    res.send("Health Care");
+	res.send("Health Care");
 });
 
 var server = app.listen(3000, function () {
-    var port = server.address().port;
-    console.log(`Health care at http://localhost:${port}`);
+	var port = server.address().port;
+	console.log(`Health care at http://localhost:${port}`);
 });
